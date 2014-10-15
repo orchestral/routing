0f38c32813b9fa11e4ea983a12c74a6e665354ca
crynobone <crynobone@gmail.com>
2014-10-15 16:36:53 +0800

    Add routing component.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-full.sh
+++
@@ -1,4 +0,0 @@
-git subsplit init git@github.com:orchestral/kernel.git
-git subsplit publish src/Contracts:git@github.com:orchestral/contracts.git
-git subsplit publish src/Http:git@github.com:orchestral/http.git
-rm -rf .subsplit/
